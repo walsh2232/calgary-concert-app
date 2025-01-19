5a679a96846aaae4cf9d73d2eefce2b040a411a0
walsh2232 <76233561+walsh2232@users.noreply.github.com>
2025-01-19 12:41:20 -0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  esbuild: {
+    // Force .js to parse as JSX if you want, though not usually needed if plugin-react is installed
+    loader: 'jsx',
+  }
 })
